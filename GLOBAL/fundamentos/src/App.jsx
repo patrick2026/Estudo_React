5ce1439b29cfae6787164c54c42cc7466c9f2f82
patrick2026 <patrickventura31@gmail.com>
2025-03-28 21:45:44 -0300

    Criando o primeiro componente

---
+++ GLOBAL/fundamentos/src/App.jsx
@@ -0,0 +1,12 @@
+import FirstComponent from "./components/FirstComponent";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ GLOBAL/fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,9 @@
+const FirstComponent = () => {
+  return (
+    <div>
+      <h1>Primeiro Componente</h1>
+    </div>
+  );
+};
+
+export default FirstComponent;
