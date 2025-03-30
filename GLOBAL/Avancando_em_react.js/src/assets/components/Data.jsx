88ae133785f292529b507832a6bf8f65e5564f9b
patrick2026 <patrickventura31@gmail.com>
2025-03-29 22:23:59 -0300

    Melhorando o  codigo

--- GLOBAL/Avancando_em_react.js/src/assets/components/Data.jsx
+++ GLOBAL/Avancando_em_react.js/src/assets/components/Data.jsx
@@ -1,20 +1,25 @@
 import { useState } from "react";
 
 const Data = () => {
-  let someData = 10;
-
-  //criando o useState - primeiro argumento e a variável, segundo argumento e a função que vai ser executada. 15 é valor inicial
+  //1° ⇒ o estado onde vou guardar onde vai armazenados meu conteudo
+  //2° ⇒ função que atualiza o estado dele
+  // Estado inicial = 15
   const [anoTherNumber, setAnoTherNumber] = useState(15);
+
+  // Função que alterna entre 15 e 20
+  const toggleNumber = () => {
+    if (anoTherNumber === 15) {
+      setAnoTherNumber(20); // Muda para 20 se era 15
+    } else {
+      setAnoTherNumber(15); // Volta para 15 se era 20
+    }
+  };
 
   return (
     <div>
       <div>
-        <p>Valor:{someData}</p>
-        <button onClick={() => (someData = 15)}>Mudar variável</button>
-      </div>
-      <div>
-        <p>Valor:{anoTherNumber}</p>
-        <button onClick={() => setAnoTherNumber(20)}>Mudar o state</button>
+        <p>Valor: {anoTherNumber}</p>
+        <button onClick={toggleNumber}>Alternar Valor</button>
       </div>
     </div>
   );
